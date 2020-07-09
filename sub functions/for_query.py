f334919a91eeac9354f7946fc1efab5c5828eda1
ngocdiep892000 <66045162+ngocdiep892000@users.noreply.github.com>
2020-07-10 01:42:27 +0700

    add glowy borderline when a county is chosen

---
+++ sub functions/for_query.py
@@ -0,0 +1,100 @@
+import pandas as pd
+#import numpy as np
+#import sklearn as skl
+#from sklearn import preprocessing
+#import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+    
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+
+data = pd.read_csv("../../../Downloads/covid_community_vulnerability-master/data/severe_cases_score_data.csv", dtype={'FIPS': str})
+#data.head(6)
+#print(data[data.State == 'New York'])
+
+indicators_lst = ['Severe COVID Case Complications',
+'covid_cases',
+'Years of Potential Life Lost Rate',
+'% Fair or Poor Health',
+'% Smokers',
+'% Adults with Obesity',
+'% Adults with Diabetes',
+'% 65 and over']
+
+all_states = list(data.State.unique())
+all_states.insert(0, "United States")
+
+
+
+fig = px.choropleth_mapbox(data, geojson=counties, locations=data.FIPS, color=data['Severe COVID Case Complications'],
+                           color_continuous_scale='Blues',
+                           range_color=(35,65),
+                           mapbox_style="carto-positron",
+                           zoom=3.5, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.1,
+                           labels={"severe_cases_score_data"},
+						hover_data = ['Severe COVID Case Complications'])
+fig.update_traces(marker={"line":{"width":2, 'color':'DarkSlateGrey'}})
+fig.update_layout(
+		title_text = 'severe_cases_score_data',
+	   	margin={"r":0,"t":0,"l":0,"b":20})
+
+dff = data[data['State']== 'Arizona']
+fig2 = px.choropleth_mapbox(dff, geojson=counties, locations=dff.FIPS, color=dff['Severe COVID Case Complications'],
+                           color_continuous_scale='Blues',
+                           range_color=(35,65),
+                           mapbox_style="carto-positron",
+                           zoom=8, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.1,
+                           labels={"severe_cases_score_data"},
+						hover_data = ['Severe COVID Case Complications'])
+fig2.update_traces(marker={"line":{"width":8, 'color':'Fuchsia'}})
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+
+#object for app
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+#CHANGE TIME PERIOD?
+app.layout = html.Div([
+	html.Div([
+		html.Div([
+			html.Label('Choose State'),
+			dcc.Dropdown(
+				id = 'choose-state',
+				options= [{'label': state, 'value': state} for state in all_states],
+				value= 'United States'
+)
+],
+	style={'width': '48%', 'display': 'inline-block'}),
+
+	html.Div([
+		html.Label('Indicator'),
+		dcc.Dropdown(
+            id='choose-indicator',
+            options=[{'label': i, 'value': i} for i in indicators_lst],
+            value='Severe COVID Case Complications'
+            )
+    ],
+    style={'width': '48%', 'display': 'inline-block'})]),
+
+    dcc.Graph(id='counties-map', figure=fig),#WHERE THE MAP WOULD BE
+  	dcc.Graph(id='counties-map2', figure=fig2)  
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
