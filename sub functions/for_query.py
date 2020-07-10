0f3e3edd08df369051d8af32d4f6bc28a711f38a
ngocdiep892000 <66045162+ngocdiep892000@users.noreply.github.com>
2020-07-10 16:21:23 +0700

    add glowy border for query

--- sub functions/for_query.py
+++ sub functions/for_query.py
@@ -1,8 +1,4 @@
 import pandas as pd
-#import numpy as np
-#import sklearn as skl
-#from sklearn import preprocessing
-#import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 from urllib.request import urlopen
 import json
@@ -18,9 +14,7 @@ from dash.dependencies import Input, Output
 
 
 data = pd.read_csv("../../../Downloads/covid_community_vulnerability-master/data/severe_cases_score_data.csv", dtype={'FIPS': str})
-#data.head(6)
-#print(data[data.State == 'New York'])
-
+long_lat = pd.read_csv('long_lat.csv')
 indicators_lst = ['Severe COVID Case Complications',
 'covid_cases',
 'Years of Potential Life Lost Rate',
@@ -33,8 +27,6 @@ indicators_lst = ['Severe COVID Case Complications',
 all_states = list(data.State.unique())
 all_states.insert(0, "United States")
 
-
-
 fig = px.choropleth_mapbox(data, geojson=counties, locations=data.FIPS, color=data['Severe COVID Case Complications'],
                            color_continuous_scale='Blues',
                            range_color=(35,65),
@@ -42,22 +34,26 @@ fig = px.choropleth_mapbox(data, geojson=counties, locations=data.FIPS, color=da
                            zoom=3.5, center = {"lat": 37.0902, "lon": -95.7129},
                            opacity=0.1,
                            labels={"severe_cases_score_data"},
-						hover_data = ['Severe COVID Case Complications'])
+						hover_data = ['County', 'State'] + indicators_lst)
 fig.update_traces(marker={"line":{"width":2, 'color':'DarkSlateGrey'}})
 fig.update_layout(
 		title_text = 'severe_cases_score_data',
 	   	margin={"r":0,"t":0,"l":0,"b":20})
 
-dff = data[data['State']== 'Arizona']
+dff = data[data['County']== 'Harris'][data['State'] == 'Texas'] 
+#dff = dff1[dff1['State'] == 'Texas']
 fig2 = px.choropleth_mapbox(dff, geojson=counties, locations=dff.FIPS, color=dff['Severe COVID Case Complications'],
                            color_continuous_scale='Blues',
                            range_color=(35,65),
                            mapbox_style="carto-positron",
-                           zoom=8, center = {"lat": 37.0902, "lon": -95.7129},
+                           zoom=5, center = {'lat' : float(long_lat[long_lat['name'] == 'Texas']['latitude']), "lon": float(long_lat[long_lat['name'] == 'Texas']['longitude'])},
                            opacity=0.1,
                            labels={"severe_cases_score_data"},
-						hover_data = ['Severe COVID Case Complications'])
+						hover_data = ['County', 'State'] + indicators_lst)
 fig2.update_traces(marker={"line":{"width":8, 'color':'Fuchsia'}})
+fig.update_layout(
+		#title_text = 'severe_cases_score_data',
+	   	margin={"r":0,"t":0,"l":0,"b":20})
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -90,10 +86,11 @@ app.layout = html.Div([
     ],
     style={'width': '48%', 'display': 'inline-block'})]),
 
-    dcc.Graph(id='counties-map', figure=fig),#WHERE THE MAP WOULD BE
+    #dcc.Graph(id='counties-map', figure=fig),#WHERE THE MAP WOULD BE
   	dcc.Graph(id='counties-map2', figure=fig2)  
 
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
