48d204393ecc87960cd8cce4bff96757badaf3f3
Diep Hoang <dvh3@rice.edu>
2021-01-14 11:07:51 +0700

    fixed bug

--- AllComponents/intro_sidebar.py
+++ AllComponents/intro_sidebar.py
@@ -1,6 +1,5 @@
 import dash
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 
 
 sidebar_header = html.Div(
@@ -24,19 +23,15 @@ sidebar = html.Div(
                     html.P([" aiming to effectively understand community needs, so that resources can be equitably allocated at the county level.\n", html.Br()]),
                     html.P(["Use this dashboard to visualize community needs assessment scores related to the impact of COVID-19, at the county level. Also map the underlying health and demographic variables used to build the scores.\n", html.Br()]),
                     html.P(["Assess county risk levels based on those scores.\n", html.Br()], className = "spacebtwn"),
-                    html.P("\n"),
                     html.P(["Choose a Location\n", html.Br()], className = "underline"),
                     html.P(["Zoom on the map or use the \"Filter by State\" and \"Filter by County\" dropdown menus above the map.\n", html.Br()], className = "spacebtwn"),
-                    html.P("\n"),
                     html.P(["Map a Metric or Variable Data Set\n", html.Br()], className= "underline"),
                     html.P(["Select a metric to map in the box on the right of the map.\n", html.Br()], className = "spacebtwn"),
-                    html.P("\n"),
                     html.P(["View Detailed Data for A County\n",html.Br()] , className = "underline"),
                     html.P(["Click on a county on the map to view COVID-19 and population census data.\n",html.Br()]),
                     html.P(["In the lower right hand corner, view the top 50 county vulnerability scores and rankings in a specific state.\
                     Use the arrows at the bottom of the module to toggle between the scores for different metrics.\
                     Change the state with the \"Filter by State\" dropdown menu above the map.\n",html.Br()], className = "spacebtwn"),
-                    html.P("\n"),
                     html.P("For more information ", className="bold"),
                     html.P("about the project, click  the Menu icon in the top right corner. Feel free to reach out to "),
                     html.A(href= "mailto: covid.vulnerability@gmail.com", children = "covid.vulnerability@gmail.com",  target="_blank", rel="noopener noreferrer"),
