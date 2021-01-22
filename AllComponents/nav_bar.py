abe45c5578d2c51758aac813c23134086adc38cd
Alex Rios <rg.alejandra.17@gmail.com>
2021-01-21 19:34:26 -0600

    nav bar changes draft

--- AllComponents/nav_bar.py
+++ AllComponents/nav_bar.py
@@ -1,21 +1,39 @@
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 #Navigation Bar at the right corner of the dashboard
+
+logo = "https://raw.githubusercontent.com/community-insight-impact/covid_community_vulnerability/master/CVI%20Logo.png"
 nav_bar = html.Div(
-    [
-    html.Div(className='navbar', 
-        children=[
-            html.Img(src='https://raw.githubusercontent.com/community-insight-impact/covid_community_vulnerability/master/CVI%20Logo.png',
-        style={'height':'28px','margin':8}), 
-            html.H2("COVID-19 Community Vulnerability Index  ", style={'font-size':18, 'vertical-align': 'middle', 'color': 'white'}),
-            html.Div(className='dropdown', 
-            children=[html.Button(className='dropbtn', children= html.Span(children=[html.P('Menu ',  style = {'display':'inline-block'}) ,#style={'margin-bottom': -10}),
-                html.I(className="fa fa-caret-down", **{'aria-hidden': 'true'}, style = {'display':'inline-block', 'marginTop': '10px', 'marginLeft':'5px'})], style = {'display':'flex'})), 
-                    html. Div(className='dropdown-content', children=
-                        [html.A('About', href='https://github.com/community-insight-impact/covid_community_vulnerability', target="_blank"), 
-                        html.A('Dataset', href='https://github.com/community-insight-impact/covid_community_vulnerability/blob/master/data/full_dataset.md', target="_blank"),
-                        html.A('Methodology', href='https://github.com/community-insight-impact/covid_community_vulnerability/blob/master/documentation/methodology.md', target="_blank")])#, style = {'display':'inline-block'})
-            ], style={'zIndex': 1000}),
-        ], style={'display':'flex', 'zIndex': 1}),
-        ]) 
+    children=[
+        dbc.Navbar(className="navbar",
+            children=[
+                html.A(
+                    # Use row and col to control vertical alignment of logo / brand
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Img(src=logo, height="30px")),
+                            dbc.Col(dbc.Navbar("COVID-19 Community Vulnerability Index")),
+                        ],
+                        align="center",
+                        no_gutters=True,
+                    ),
+                ),
+                dbc.NavbarToggler(id="navbar-toggler"),
+            # dbc.Collapse(nav_bar, id="navbar-collapse", navbar=True),
+            ],
+            color="dark",
+            dark=True,
+        ),
+        dbc.DropdownMenu(className="dropdown",
+                    label='Menu',
+                    in_navbar=True,
+                    children=[
+                        dbc.DropdownMenuItem('About', href='https://github.com/community-insight-impact/covid_community_vulnerability', target="_blank"), 
+                        dbc.DropdownMenuItem('Dataset', href='https://github.com/community-insight-impact/covid_community_vulnerability/blob/master/data/full_dataset.md', target="_blank"),
+                        dbc.DropdownMenuItem('Methodology', href='https://github.com/community-insight-impact/covid_community_vulnerability/blob/master/documentation/methodology.md', target="_blank"),#, style = {'display':'inline-block'})
+                    ],
+            ),
+    ],
+)
 
